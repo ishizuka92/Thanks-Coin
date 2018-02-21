c27a38092a14c87ec64ff0d7a9c5d33af19512f3
sho0001 <35943729+sho0001@users.noreply.github.com>
2018-02-21 17:26:23 +0900

    管理者履歴画面のメッセージダイアログ化

--- src/app/admin/transactions/transactions.component.ts
+++ src/app/admin/transactions/transactions.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatTableDataSource
+      , MatDialog
+      , MatDialogRef
+      , MAT_DIALOG_DATA} from '@angular/material';
 import {TransactionsService} from './transactions.service';
+import {MessageDialog} from '../../common/message-dialog.component';
+
 
 /**
  * @title Table with pagination
@@ -14,11 +19,20 @@ import {TransactionsService} from './transactions.service';
 export class TransactionsComponent{
 
   // サービス利用の宣言
-  constructor(private service: TransactionsService) { }
+  constructor(private service: TransactionsService,
+              public dialog: MatDialog) { }
 
   // 表示項目の取得
   displayedColumns = this.service.getDisplayColumns();
   // リストの取得
   dataSource = this.service.getDataSource();
 
+  // メッセージ用ダイアログ
+  // ボタン押下時の処理
+  openDialog(message:string) {
+    let dialogRef = this.dialog.open(MessageDialog, {
+      width: '500px',
+      data: { messageDisp: message}
+    });
+  }
 }
